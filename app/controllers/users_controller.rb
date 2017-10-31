be29d29dffd0b5581b65ef6e7ad29d2ca3caa966
serpilcc <serpil60@hotmail.fr>
2017-10-31 02:21:52 +0100

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+def index
+end
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,6 +14,8 @@ class UsersController < ApplicationController
   def create
   	@user=User.new(user_params)
   	if @user.save
+      log_in @user
+      redirect_to @user
   	else
   		render 'new'
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get  '/signup',  to: 'users#new'
 
   resources :users
+
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 
 
   
