162372fe078c4d5adf7b895ca029d63f71c2e73d
serpilcc <serpil60@hotmail.fr>
2017-10-31 01:04:29 +0100

    members only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-get  '/signup',  to: 'users#new'
+  get 'users/new'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get  '/signup',  to: 'users#new'
 
-resources :users
+  resources :users
 
+
+  
 end
+
